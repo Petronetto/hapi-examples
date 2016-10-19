06732136248b59455372b9bea90c4b0eb2a04b0e
Juliano Petronetto <juliano@petronetto.com.br>
2016-10-19 10:48:11 -0200

    Section 2/9

--- index.js
+++ index.js
@@ -4,64 +4,54 @@ const Hapi = require('hapi')
 
 // Create server and connection
 const server = new Hapi.Server()
-
 server.connection({
   port: 3000
 })
+
+// const pre1 = (request, reply) => {
+//   return reply('Hello world')
+// }
+
+server.method('m1', (request, reply) => {
+  return reply('Hello world')
+})
+
+const pre2 = (request, reply) => {
+  return reply(' from HapiJs!')
+}
+
+const pre3 = (request, reply) => {
+  return reply(request.pre.m1 + request.pre.m2)
+}
 
 // Creating routes
 server.route({
   method: 'GET',
   path: '/',
   handler: (request, reply) => {
-    return reply('Hello World!')
+    return reply(request.pre.m3)
+  },
+  config: {
+    pre: [
+      [{
+        method: 'm1'
+      }, {
+        method: pre2,
+        assign: 'm2'
+      }], {
+        method: pre3,
+        assign: 'm3'
+      }
+    ]
   }
 })
 
-server.route({
-  method: 'GET',
-  path: '/json',
-  handler: (request, reply) => {
-    return reply({
-      message: 'Hello World (JSON)'
-    })
-  }
-})
-
-// Register Good plugin and start the server
-server.register({
-  register: require('good'),
-  options: {
-    reporters: {
-      console: [
-        {
-          module: 'good-squeeze',
-          name: 'Squeeze',
-          args: [
-            {
-              log: '*',
-              response: '*'
-            }
-          ]
-        },
-        {
-          module: 'good-console',
-        },
-        'stdout'
-      ]
-    }
-  }
-},(err) => {
+server.start((err) => {
   if (err) {
     throw err
   }
-
-  // Start the server
-  server.start((err) => {
-    if (err) {
-      throw err
-    }
-
-    console.log('Server is running at: ', server.info.uri)
-  })
+  console.log('Server running at: ', server.info.uri)
 })
+
+
+
